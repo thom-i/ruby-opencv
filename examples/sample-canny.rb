5813e2f1c8ca640be4cf1caf6c12bb471f28af62
ishimaru_tomoki <ishimaru-tomoki@gmo-ap.jp>
2016-11-28 12:51:23 +0900

    Folder organization

--- examples/sample-canny.rb
+++ examples/sample-canny.rb
@@ -1,7 +1,7 @@
 require 'opencv'
 include OpenCV
 
-src_img = CvMat.load("Lenna.bmp")
+src_img = CvMat.load("../image/Lenna.bmp")
 canny_img = src_img.BGR2GRAY.canny(120,200)
 
 GUI::Window.new('Input').show(src_img)
--- examples/sample-template.rb
+++ examples/sample-template.rb
@@ -1,14 +1,14 @@
 require 'opencv'
 include OpenCV
 
-src_img = CvMat.load('cat_sample.jpg')
-tmp_img = CvMat.load('cat_tmp.jpg')
+src_img = CvMat.load('../image/char_sample.png')
+tmp_img = CvMat.load('../image/char_tmp.png')
 
 result = src_img.match_template(tmp_img, CV_TM_SQDIFF)
 
 pt1 = result.min_max_loc[2]
 pt2 = CvPoint.new(pt1.x + tmp_img.width, pt1.y + tmp_img.height)
-result_img = src_img.rectangle(pt1, pt2, :color => CvColor::Black, :thickness => 3)
+result_img = src_img.rectangle(pt1, pt2, :color => CvColor::Red, :thickness => 3)
 
 GUI::Window.new('src_img').show(src_img)
 GUI::Window.new('tmp_img').show(tmp_img)
